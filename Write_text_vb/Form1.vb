75bdfbe6e4028acb675a0250a08c5db97b106767
Fakhruddin90 <Fakhruddin90.com>
2020-05-18 16:32:11 +0800

    initial commit

---
+++ Write_text_vb/Form1.vb
@@ -0,0 +1,9 @@
+﻿Imports System.IO
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim filename As String = Application.StartupPath & "\test.log"
+        Dim sw As StreamWriter = File.AppendText(filename)
+        sw.WriteLine(Now() & "" & " sample log file entry")
+        sw.Close()
+    End Sub
+End Class
